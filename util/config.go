222a2225a7509be90438f89e528a6df4e90349b2
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-06-16 12:57:25 +0700

    1st SQLC

--- util/config.go
+++ util/config.go
@@ -3,9 +3,9 @@ package util
 import "github.com/spf13/viper"
 
 type Config struct {
-	DBDriver      string `mpastructure:"DBDriver"`
-	DBSource      string `mpastructure:"DBSource"`
-	ServerAddress string `mpastructure:"ServerAddress"`
+	DBDriver      string `mapstructure:"DBDriver"`
+	DBSource      string `mapstructure:"DBSource"`
+	ServerAddress string `mapstructure:"ServerAddress"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
@@ -15,5 +15,14 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
+	if err != nil {
+		return config, err
+	}
 
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
