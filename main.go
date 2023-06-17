9b4baa22cfe9cc938e64952e0a8ef5c93a044f95
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-06-17 12:43:08 +0700

    refactor by gpt

--- main.go
+++ main.go
@@ -15,28 +15,30 @@ func main() {
 	g := gin.Recovery()
 	fmt.Println(g)
 
-	db_string := "host=localhost port=5432 user=postgres password=secret dbname=knex_db sslmode=disable"
+	dbString := "host=localhost port=5432 user=postgres password=secret dbname=knex_db sslmode=disable"
 
-	db, err := sql.Open("postgres", db_string)
+	dbConn, err := sql.Open("postgres", dbString)
 	if err != nil {
 		log.Fatal("failed to connect to the database:", err)
 	}
-	defer db.Close()
+	defer dbConn.Close()
 
 	ctx := context.Background()
 
-	createAccountTest(ctx, db)
+	// Initialize the db.Queries struct
+	queries := db.New(dbConn)
 
+	createAccountTest(ctx, queries)
 }
 
-func createAccountTest(ctx context.Context, db *db.Queries) {
+func createAccountTest(ctx context.Context, queries *db.Queries) {
 	params := db.CreateAccountParams{
 		Owner:    "John Doe",
 		Balance:  1000,
 		Currency: "USD",
 	}
 
-	account, err := db.CreateAccount(ctx, params)
+	account, err := queries.CreateAccount(ctx, params)
 	if err != nil {
 		log.Println("failed to create account:", err)
 		return
