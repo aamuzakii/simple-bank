2a50eb234b344cf0671392da2215b53bacfe25d2
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-06-17 12:45:08 +0700

    last refactor by GPT OK

--- main.go
+++ main.go
@@ -25,24 +25,45 @@ func main() {
 
 	ctx := context.Background()
 
-	// Initialize the db.Queries struct
-	queries := db.New(dbConn)
-
-	createAccountTest(ctx, queries)
+	createAccountTest(ctx, dbConn)
 }
 
-func createAccountTest(ctx context.Context, queries *db.Queries) {
-	params := db.CreateAccountParams{
-		Owner:    "John Doe",
-		Balance:  1000,
-		Currency: "USD",
-	}
-
-	account, err := queries.CreateAccount(ctx, params)
+func createAccountTest(ctx context.Context, dbConn *sql.DB) {
+	tx, err := dbConn.Begin()
 	if err != nil {
-		log.Println("failed to create account:", err)
+		log.Fatal("failed to begin transaction:", err)
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p) // re-throw panic after rollback
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+			if err != nil {
+				log.Fatal("failed to commit transaction:", err)
+			}
+		}
+	}()
+
+	var account db.Account
+	err = tx.QueryRowContext(ctx, "SELECT id, owner, balance, currency, created_at FROM accounts WHERE id = $1 LIMIT 1", 1).Scan(
+		&account.ID,
+		&account.Owner,
+		&account.Balance,
+		&account.Currency,
+		&account.CreatedAt,
+	)
+	if err != nil {
+		log.Println("failed to retrieve account:", err)
 		return
 	}
 
-	fmt.Println("Created account:", account)
+	fmt.Println("Retrieved account:", account)
+
+	// Other database operations within the transaction can be performed here
+	// ...
+
+	// If everything is successful, the transaction will be committed at the end
 }
